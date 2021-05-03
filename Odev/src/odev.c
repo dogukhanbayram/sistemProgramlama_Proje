9097a1eeacc2e17b4177cbee50533d7d020b5f59
Bedirhan Güner <bedirhanguner@hotmail.com>
2021-05-03 12:43:59 +0300

    kilit parser düzeltildi. encrpyt ve decrypt işlemlerinin yapısı oluşturuldu.

--- Odev/src/odev.c
+++ Odev/src/odev.c
@@ -4,6 +4,7 @@
 #include "fields.h"
 #include "jrb.h"
 #include "jval.h"
+#include <fcntl.h>
 
 int main(int argc, char **argv)
 {
@@ -11,17 +12,29 @@ int main(int argc, char **argv)
   JRB t,tmp;
   IS is;
   int i;
-  char *kilit[20];
-  for(i=0;i<20;i++)
+  int size = 500;
+  char *kilit[size];
+  
+  IS is2;
+
+  if (argc != 2) { fprintf(stderr, "usage: printwords filename\n"); exit(1); }
+  
+  
+  for(i=0;i<size;i++)
   {
-    kilit[i] = NULL;
+	kilit[i] = NULL;
   }
-  //if (argc != 2) { fprintf(stderr, "usage: printwords filename\n"); exit(1); }
- 
+  
   /* Open the file as an inputstruct.  Error check. */
 
-  is = new_inputstruct(".kilit");
+  is = new_inputstruct("data/.kilit");
   if (is == NULL) {
+	perror(argv[1]);
+	exit(1);
+  }
+  
+  is2 = new_inputstruct(argv[1]);
+  if (is2 == NULL) {
     perror(argv[1]);
     exit(1);
   }
@@ -33,14 +46,15 @@ int main(int argc, char **argv)
   char *acp;char *kapap;
   while(get_line(is) >= 0) {
     for (i = 0; i < is->NF; i++) {
-        acp = strchr(is->fields[i],'{');kapap = strchr(is->fields[i],'}');
-        if(acp == NULL && kapap == NULL){
-        kilit[len] = strdup(is->fields[i]);
-        //printf("%d: %s\n", is->line, is->fields[i]);
-        len++;
+	acp = strchr(is->fields[i],'{');kapap = strchr(is->fields[i],'}');
+	if(acp == NULL && kapap == NULL){
+		kilit[len] = strdup(is->fields[i]);
+		//printf("%d: %s\n", is->line, is->fields[i]);
+		len++;
         }
     }
   }
+
 
 //parsing kilit
   int x=0; int y=0;
@@ -49,16 +63,6 @@ int main(int argc, char **argv)
     while(kilit[x][y] != '\0')
     {
       kilit[x][y] = kilit[x][y+1];
-      y++;
-    }
-    x++;
-    y=0;
-  }
-  x=0;y=0;
-    while(kilit[x] != NULL)
-  {
-    while(kilit[x][y] != '\0')
-    {
       if(kilit[x][y]=='"' || kilit[x][y]==',' || kilit[x][y]==':')
       {
         kilit[x][y]='\0';
@@ -68,22 +72,36 @@ int main(int argc, char **argv)
     x++;
     y=0;
   }
+ 
 //çift elemanlar kelime, tek elemanlar şifrelenmiş hal
-
   int k=0;
   while(k < len)
   {
-  jrb_insert_str(t,kilit[k+1],new_jval_s((char *)kilit[k]));
-  k+=2;
+  //Encrypt işlemi için aşağıdaki satırla değiştirip programı bin/odev data/decripted şeklinde çalıştırabiliriz
+
+  //jrb_insert_str(t,kilit[k],new_jval_s((char *)kilit[k+1])); 
+	jrb_insert_str(t,kilit[k+1],new_jval_s((char *)kilit[k]));
+	k+=2;
   }
   k=0;
+  
   while(k < len)
   {
-  printf("%s\n", kilit[k]);
-  k++;
+	//printf("%s\n", kilit[k]);
+	k++;
   }
-  tmp = jrb_find_str(t,"0");
-  printf("%s\n", tmp->val.s);
-  jettison_inputstruct(is);
+  int j;
+  while(get_line(is2) >= 0) {
+    for (i = 0; i < is2->NF; i++) {
+    	if(tmp = jrb_find_str(t,is2->fields[i])){
+  	  printf("%s ", tmp->val.s);
+  	}
+
+    }
+  }
+  
+jettison_inputstruct(is2);
+jettison_inputstruct(is);
+  
   return 0;
 }
